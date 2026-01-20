36d5ce2c58b5615ace6a0492f591631d449a885c
Vu Manh <vungocmanh23111999@gmail.com>
2026-01-20 23:15:53 +0900

    first commit

---
+++ src/main/java/com/example/md4bt2/Md4Bt2Application.java
@@ -0,0 +1,13 @@
+package com.example.md4bt2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Md4Bt2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Md4Bt2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/md4bt2/Md4Bt2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.md4bt2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Md4Bt2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
